fddadc54fbebb9fa12edcdbc619697e313bb9697
Laila Pegoraro <53625673+lailapegoraro@users.noreply.github.com>
2020-04-21 18:06:51 -0300

    Create exercicio1_nota.py

---
+++ exercicio1_nota.py
@@ -0,0 +1,14 @@
+"""
+Exercício de Programação - Iniciante - Python
+Laila Pegoraro 11 de abril de 2020
+
+Faça um programa que receba duas notas digitadas pelo usuário. Se a nota for maior ou igual a seis, escreva aprovado. Se não escreva reprovado.
+
+"""
+
+nota = float (input ("Digite sua nota:"))
+
+if nota >= 6:
+	print ("Aprovado")
+else:
+	print ("Reprovado")
