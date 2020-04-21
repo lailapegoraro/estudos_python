db9c122e79151e696621015c5b9a432b2a6e1983
Laila Pegoraro <53625673+lailapegoraro@users.noreply.github.com>
2020-04-21 18:05:45 -0300

    Create exercicio1_idade.py

---
+++ exercicio1_idade.py
@@ -0,0 +1,14 @@
+"""
+Exercício de Programação - Iniciante - Python
+Laila Pegoraro 11 de abril de 2020
+
+Faça um programa que receba a idade do usuário e diga se ele é maior ou menor de idade
+
+"""
+
+idade = int (input ("Qual sua idade?"))
+
+if idade >= 18:
+	print ("Maior de idade!")
+else:
+	print ("Menor de idade")
